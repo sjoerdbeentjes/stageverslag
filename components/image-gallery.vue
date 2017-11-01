0d451650a3ff5dc15f4f5eccb9667a96e3133e91
Sjoerd Beentjes <sjoerd@startmail.com>
2017-11-01 14:06:19 +0100

    implemented image gallery

---
+++ components/image-gallery.vue
@@ -0,0 +1,72 @@
+<template>
+  <section class="image-gallery">
+    <img @click="openLightbox(image)" v-for="image in images" :key="image.alt" :src="`${image.url}w=200&h=200&fit=crop`" :alt="image.alt">
+
+    <div @click="closeLightbox" class="lightbox" v-if="lightboxIsOpen">
+      <img :src="currentImage.url" :alt="currentImage.alt">
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      lightboxIsOpen: false,
+      currentImage: {}
+    }
+  },
+  props: {
+    images: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    openLightbox(image) {
+      this.lightboxIsOpen = true;
+      this.currentImage = image;
+    },
+    closeLightbox() {
+      this.lightboxIsOpen = false;
+      this.currentImage = {};
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .image-gallery {
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    margin-top: 2em;
+
+    > img {
+      display: block;
+      width: 8em;
+      height: 8em;
+      margin-right: 1em;
+      margin-bottom: 1em;
+      cursor: pointer;
+    }
+
+    .lightbox {
+      position: fixed;
+      height: 100%;
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      top: 0;
+      left: 0;
+      background-color: rgba(0, 0, 0, 0.5);
+
+      img {
+        max-width: 90%;
+        max-height: 90%;
+      }
+    }
+  }
+</style>
+
